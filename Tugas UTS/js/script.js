36799c8febecc0409aa8cb5aa3bce119252cee94
yupapratini <yulfapratini15@gmail.com>
2024-11-17 05:54:10 +0700

    Add files via upload

---
+++ Tugas UTS/js/script.js
@@ -0,0 +1,7 @@
+// Toggle class active 
+const navbarNav = document.querySelector
+('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hanburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
